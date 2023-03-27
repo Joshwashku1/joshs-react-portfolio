5aa85afb091d32de99c7ffec172caa3637ca0c09
JoshuaHamann <joshwashku@yahoo.com>
2023-03-27 15:24:06 -0500

    added a currentPage state to the portfolio container component

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+
+    // set a state and default it home
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    // Method to check what the current state is
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+            return <Home />;
+        }
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if(currentPage === 'Blog') {
+            return <Blog />;
+        }
+        return <Contact />;
+    }
+
+    // This function will pass in the a page variable then set the state to the page
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            {renderPage()}
+        </div>
+    )
+}
